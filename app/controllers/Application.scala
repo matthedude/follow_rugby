4a757f44f7b1c6a3760ae58a628faab67b857060
Matthew Pickering <matthedude@hotmail.com>
2013-05-14 22:02:17 +0100

    Deleted competition and did rubbish hack to allow for rugbyunited hashtag widget to appear in category

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,63 +11,10 @@ import anorm._
 object Application extends Controller {
   
 	val Home = Redirect(routes.Application.index)
-	
-	val lionsCompForm = Form(
-    (mapping(
-      "props" -> list(text),
-      "hookers" -> list(text),
-      "locks" -> list(text),
-      "backRows" -> list(text),
-      "scrumHalfs" -> list(text),
-      "flyHalfs" -> list(text),
-      "centres" -> list(text),
-      "wings" -> list(text),
-      "fullBacks" -> list(text),
-      "name" -> nonEmptyText,
-      "email" -> (nonEmptyText verifying email.constraints.head),
-      "password" -> nonEmptyText,
-      "phone" -> nonEmptyText
-    )(LionsCompTeam.apply)(LionsCompTeam.unapply)
-  ))
-  
-  val loadCompForm = Form(
-    tuple(
-      "email" -> text,
-      "password" -> text
-    ) verifying ("Invalid email or password", result => result match {
-      case (email, password) => CompEntry.loadEntry(email, password).isDefined
-    })
-  )
   
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
-	
-//	def enterCompetition = Action { implicit request =>
-//    lionsCompForm.bindFromRequest.fold(
-//      formWithErrors => 
-//        BadRequest(views.html.competition(LionsCompPlayer.all)(Nil)(formWithErrors.withGlobalError("You have made an error filling out the form. Please make sure you have supplied your name, a valid email and a telephone number."))(loadCompForm)),
-//      compEntry => {
-//      	import java.util.Date
-//        val players = (compEntry.props ++ compEntry.hookers ++ compEntry.locks ++ compEntry.backRows ++ compEntry.scrumHalfs ++ compEntry.flyHalfs ++ compEntry.centres ++ compEntry.wings ++ compEntry.fullBacks).mkString(",")
-//      	val entry = CompEntry(compEntry.name, compEntry.email, compEntry.password, compEntry.phone, players)
-//      	val compSelectedPlayers = LionsCompPlayer.findByIds(players).map(_.name)
-//        if(CompEntry.findByEmail(compEntry.email) == None) {
-//          CompEntry.create(entry)
-//          Email.sendEnterConfirmationMail(compSelectedPlayers, compEntry)
-//      	} else {
-//      		val updated = CompEntry.update(entry)
-//      		if(!updated) {
-//      			Home.flashing("error" -> "There was a problem entering the competition, please try again. If you have a saved squad then re-load it.")
-//      		} else {
-//      			Email.sendUpdateConfirmationMail(compSelectedPlayers, compEntry)
-//      		}
-//      	}
-//        Home.flashing("success" -> "You have successfully entered the Follow Rugby Lions competition. Please check your email shortly for a confirmation email.")
-//      }
-//    )
-//  }
-	
   
   def about = Action {
     Ok(views.html.about())
@@ -77,12 +24,16 @@ object Application extends Controller {
     Ok(views.html.matchCentre(Game.findByCompetitionId(id))(Competition.findById(id)))
   }
   
-  def selectCategory(id: Int, categoryName: String) = Action {
+ def selectCategory(id: Int, categoryName: String) = Action {
     val category = Category.findById(id).get
     val teams = Team.findByCategoryId(id)
     val widget = category.widgetId map (wId => Widget.findById(wId).get)
     
-    Ok(views.html.categories(category)(teams)(None)(Nil)(widget))
+    val (widget1, searchWidget) = {
+      if(id == 10) (None, widget) else (widget, None)
+    }
+    
+    Ok(views.html.categories(category)(teams)(None)(Nil)(widget1)(searchWidget))
   }
   
   def selectTeam(categoryId: Int, teamId: Int, categoryName: String, teamName: String) = Action {
@@ -93,31 +44,10 @@ object Application extends Controller {
     
     val widget = Widget.findById(selectedTeam.get.widgetId)
     
-    Ok(views.html.categories(category)(teams)(selectedTeam)(members)(widget))
+    Ok(views.html.categories(category)(teams)(selectedTeam)(members)(widget)(None))
   }
   
   def comingSoon = Action {
     Ok(views.html.comingSoon())
-  }
-  
-  def competition = Action { implicit request =>
-  	Ok(views.html.competition(LionsCompPlayer.all)(Nil)(lionsCompForm)(loadCompForm))
-  }
-  
-  def loadCompetition = Action { implicit request =>
-    loadCompForm.bindFromRequest.fold(
-      formWithErrors => 
-        BadRequest(views.html.competition(LionsCompPlayer.all)(Nil)(lionsCompForm)(formWithErrors)),
-      login => {
-        val entry = CompEntry.loadEntry(login._1, login._2)
-        if(entry.isDefined) {
-          val compSelectedPlayers = LionsCompPlayer.findByIds(entry.get.players)
-          val compOtherPlayers = LionsCompPlayer.findNotByIds(entry.get.players)
-          Ok(views.html.competition(compOtherPlayers)(compSelectedPlayers)(lionsCompForm.fill(LionsCompTeam.entryToTeam(entry.get)))(loadCompForm))
-        } else {
-        	Ok(views.html.competition(LionsCompPlayer.all)(Nil)(lionsCompForm)(loadCompForm))
-        }
-      }
-    )
   }
 }
--- app/models/Email.scala
+++ app/models/Email.scala
@@ -7,65 +7,15 @@ import scala.concurrent.Future
 
 object Email {
 	val mail = use[MailerPlugin].email
-  def sendEnterConfirmationMail(players: Seq[String], entry: LionsCompTeam) = {
+  def sendEnterConfirmationMail() = {
   	val futureInt: Future[Unit] = scala.concurrent.Future {
 	    mail.setSubject("Follow-Rugby.com British and Irish Lions 2013 squad competition Entry")
-    mail.addRecipient(entry.name + " <" + entry.email + ">")
-    mail.addFrom("Follow Rugby <ap@follow-rugby.com>")
+//    mail.addRecipient(entry.name + " <" + entry.email + ">")
+//    mail.addFrom("Follow Rugby <ap@follow-rugby.com>")
     //sends html
-    mail.sendHtml("""<html><body>
-  <p> Thank you for entering the www.Follow-Rugby.com British and Irish Lions 2013 squad competition.</p>
-
-<p>You have chosen the following players: </p>
-    
-<ul>
-""" + players.sorted.mkString("<li>", "</li><li>", "</li>") + """   
-</ul>
-
-<p>Please remember that you have until 10pm (UK BST) on April 25th to make changes to your selection. </p>
-
-<p>Please forward this e-mail to your rugby obsessed friends and see who gets closest to Warren Gatland's chosen tour party!</p>
-
-<p>Enjoy the site</p>
-
-<p><a href="www.follow-rugby.com">www.Follow-Rugby.com</a></p>
-
-<p><a href="https://twitter.com/intent/follow?original_referer=https://twitter.com/about/resources/buttons&region=follow_link&screen_name=FollowRugbySite&tw_p=followbutton&variant=2.0">@FollowRugbySite</a></p></body></html>""" )
-
-	  }
+//   mail.sendHtml
    
     
-  }
-  
-  def sendUpdateConfirmationMail(players: Seq[String], entry: LionsCompTeam)  = {
-   val futureInt: Future[Unit] = scala.concurrent.Future {
-    mail.setSubject("Follow-Rugby.com British and Irish Lions 2013 squad competition Squad Change")
-    mail.addRecipient(entry.name + " <" + entry.email + ">")
-    mail.addFrom("Follow Rugby <ap@follow-rugby.com>")
-    val html = """<html><body>
-  <p>Thank you updating your entry to the www.Follow-Rugby.com British and Irish Lions 2013 squad competition.</p>
-
-<p>You have chosen the following players: </p>
-    
-<ul>
-""" + players.sorted.mkString("<li>", "</li><li>", "</li>") + """   
-</ul>
-
-<p>Please remember that you have until 10pm (UK BST) on April 25th to make changes to your selection. </p>
-
-<p>Please forward this e-mail to your rugby obsessed friends and see who gets closest to Warren Gatland's chosen tour party!</p>
-
-<p>Enjoy the site</p>
-
-<p><a href="www.follow-rugby.com">www.Follow-Rugby.com</a></p>
-
-<p><a href="https://twitter.com/intent/follow?original_referer=https://twitter.com/about/resources/buttons&region=follow_link&screen_name=FollowRugbySite&tw_p=followbutton&variant=2.0">@FollowRugbySite</a></p></body></html>"""
-//sends html
-
-    mail.sendHtml(html)
-
-   }
-
-
-  }
+    }
+	}
 }
