dd49390d024d30e366858791a47482b38f7b6cd6
Matthew Pickering <matthedude@hotmail.com>
2013-05-16 23:50:03 +0100

    Refactored out Widgets.

--- app/controllers/Administrator.scala
+++ app/controllers/Administrator.scala
@@ -45,67 +45,9 @@ object Administrator extends Controller with Secured {
     )
   }
   
-  
-  
-  val WidgetHome = Redirect(routes.Administrator.widgetList(0, 2, ""))
-  
-  val widgetForm = Form(
-    mapping(
-      "id" -> longNumber,
-      "twitterAccount" -> nonEmptyText,
-      "url" -> nonEmptyText
-    )(Widget.apply)(Widget.unapply)
-  )
-  
   def admin = IsAuthenticated { _ => _ =>
     Ok(views.html.admin.tables())
   }
-  
-  def widgetList(page: Int, orderBy: Int, filter: String) = IsAuthenticated { _ => implicit request =>
-    Ok(views.html.admin.widgetList(
-      Widget.list(page = page, orderBy = orderBy, filter = ("%"+filter+"%")),
-      orderBy, filter
-    ))
-  }
-  
-  def createWidget = IsAuthenticated { _ => _ =>
-    Ok(views.html.admin.createWidget(widgetForm))
-  }
-  
-  def editWidget(id: Long) = IsAuthenticated { _ => _ =>
-    Widget.findById(id).map { widget =>
-      Ok(views.html.admin.editWidget(id)(widgetForm.fill(widget)))
-    } .getOrElse(NotFound)
-  }
-  
-  def saveWidget = IsAuthenticated { _ => implicit request =>
-    widgetForm.bindFromRequest.fold(
-      formWithErrors => 
-        BadRequest(views.html.admin.createWidget(formWithErrors)),
-      widget => {
-        Widget.create(widget)
-        WidgetHome.flashing("success" -> "Widget %s has been created".format(widget.id))
-      }
-    )
-  }
-  
-  def updateWidget(id: Long) = IsAuthenticated { _ => implicit request =>
-    widgetForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(views.html.admin.editWidget(id)(formWithErrors)),
-      widget => {
-        println(widget)
-        Widget.update(id, widget)
-        WidgetHome.flashing("success" -> "Widget %s has been updated".format(widget.id))
-      }
-    )
-  }
-  
-  def deleteWidget(id: Long) = IsAuthenticated { _ => _ =>
-    Widget.delete(id)
-    WidgetHome.flashing("success" -> "Widget has been deleted")
-  }
-  
-  
   
 }
 
